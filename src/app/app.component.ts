148a051c0a38976a66efa1f6ddda20694e7b524c
Manuelfsk <fonsecafernando88@gmail.com>
2024-10-03 15:45:33 -0500

    proyecto terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,15 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-
   title = 'WeldDesign';
 
-  PSI: number | null = null;
+  PSI: number | null = null
   MPA: number | null = null;
-  THETA: number | null = null;
+  THETA: number | null = null
+  RHO: number | null = null;
+  wPie: number | null = null
+  mili: number | null = null;
+  Ri: number | null = null
   resultadoFv: number | null = null;
+  resulFvi: number | null = null;
+  resmDelta: number | null = null;
+  resUDelta: number | null = null;
+  resulRHO: number | null = null;
+  resulRcrit: number | null = null;
+  resulIDelta: number | null = null;
+  RhoResultado: number | null = null;
+  frho: number | null = null;
+  Fvi: number | null = null;
+  comp_Fvix: number | null = null;
+  comp_Fviy: number | null = null;
+  Fvx: number | null = null;
+  Fvy: number | null = null;
 
-
+  /////////////////////////////////////////////
   // Método para convertir de Psi a Mpa
   ConvertirPsiaMpa() {
     if (this.PSI === null) {
@@ -28,7 +45,7 @@ export class AppComponent {
     console.log(this.PSI);
     this.CalEsfuerzoFv()
   }
-
+  /////////////////////////////////////////////
   //Método para calcular Fv
   CalEsfuerzoFv() {
     if (this.MPA === null) {
@@ -46,10 +63,80 @@ export class AppComponent {
     console.log('mpa', this.MPA);
     console.log('fv', this.resultadoFv);
     console.log('theta', this.THETA);
+  }
+  ///////////////////////////////////////////////
+  // convertir pulgada a milimetro
+  ConverIncheamili() {
+    if (this.wPie === null) {
+      console.error("no se muesta el wpie");
+      return;
+    }
+    const number_Wpie = this.wPie !== null ? Number(this.wPie) : 0
+    const incheAmili = 25.4;
+    this.mili = parseFloat((number_Wpie * incheAmili).toFixed(4))
+    console.log('mm: ', this.ConverIncheamili);
+    console.log('wpie', number_Wpie);
+  }
+  //////////////////////////////////////////////
+  // relación de la deformación del elemento “i” a la deformación en un elemento a máximo esfuerzo ρ
+  deformacionRHO() {
+    if (this.Ri === null) {
+      return;
+    }
+    if (this.THETA === null) {
+      return;
+    }
+    if (this.mili === null) {
+      return;
+    }
+    // Convertir los valores a número si no son nulos y son cadenas
+    const numbtheta = this.THETA !== null ? Number(this.THETA) : 0
+    const numbermili = this.mili !== null ? Number(this.mili) : 0
+    const number_Ri = this.Ri !== null ? Number(this.Ri) : 0
 
+    this.resmDelta = parseFloat((0.209 * Math.pow(numbtheta + 6, -0.32) * numbermili).toFixed(4));
+    this.resUDelta = parseFloat((1.087 * Math.pow((numbtheta + 6), -0.65) * numbermili).toFixed(4));
+
+    this.resulRcrit = parseFloat((this.resUDelta / number_Ri).toFixed(4));
+    this.resulIDelta = parseFloat((number_Ri * this.resUDelta / this.resulRcrit).toFixed(4));
+
+    this.RhoResultado = parseFloat((this.resulIDelta / this.resmDelta).toFixed(4))
+    console.log('theta', this.THETA);
+    console.log('Ri', number_Ri);
+    console.log('mili', this.mili);
+    console.log('mDelta', this.resmDelta);
+    console.log('uDelta', this.resUDelta);
+    console.log('Rcrit', this.resulRcrit);
+    console.log('iDelta', this.resulIDelta);
+    console.log('p', this.RhoResultado);
+    console.log('exponente -0.32', Math.pow(numbtheta + 6, -0.32));
+    console.log('theta + 6', numbtheta + 6,);
+    console.log('number theta', numbtheta);
+
+    // falta agragar la formula F(p) F(ρ) = [ρ (1.9 – 0.9ρ)]0.3
+  }
+  ///////////////////////////////////////////
+  // calcular esfuerzo Fvi
+  calcularFvi() {
+    if (this.RhoResultado === null) {
+      return;
+    }
+    if (this.resultadoFv === null) {
+      return;
+    }
+    if (this.THETA === null) {
+      return;
+    }
+    this.frho = parseFloat(Math.pow(this.RhoResultado * (1.9 - 0.9 * this.RhoResultado), 0.3).toFixed(4))
+    console.log('f(p)-->', this.frho);
+    this.Fvi = parseFloat((this.resultadoFv * this.frho).toFixed(4))
+    console.log('Fvi-->', this.Fvi);
+    this.comp_Fvix = parseFloat((this.Fvi * Math.cos(this.THETA)).toFixed(4))
+    this.comp_Fviy = parseFloat((this.Fvi * Math.sin(this.THETA)).toFixed(4))
+    this.Fvx = this.comp_Fvix
+    this.Fvy = this.comp_Fviy
 
   }
-
 
 
 }
--- src/app/validacion .solonumeros.ts
+++ src/app/validacion .solonumeros.ts
@@ -11,17 +11,17 @@ export class solonumeros implements Validator {
         const valor = control.value;
 
         // si el valor es null o undefined, no hay error
-        if (valor == null || valor == undefined || valor == "" || !valor) {
-            console.log(valor);
-            console.log("--->si el valor es null");
+        if (valor == null || valor == undefined || valor == "") {
+
+            console.log("--->si el valor es null", valor);
             return null;
         }
-        // expresión regular para validar solo numeros (positivos y sin decimales)
-        const regex = /^[0-9]+$/;
+        // expresión regular para validar solo numeros (positivos y con decimales)
+        const regex = /^[0-9]+(\.[0-9]*)?$/;
+
         // si el valor cumple con la expresión regular, es valido
         if (regex.test(valor)) {
-            console.log(valor);
-            console.log("--- si cumple con la expresion solo num");
+            console.log("--- si cumple con la expresion solo num", valor);
             return null
         } else {
             // si no cumple, retorna el error
@@ -32,9 +32,28 @@ export class solonumeros implements Validator {
     @HostListener('keypress', ['$event'])
     onKeyPress(event: KeyboardEvent) {
         const charCode = event.charCode;
-        // Permitir solo caracteres numéricos (0-9) y bloquear otros
-        if (charCode < 48 || charCode > 57) {
+        const inputChar = String.fromCharCode(charCode);
+
+        // Permitir solo números (0-9) y punto decimal (.), y bloquear otros caracteres
+        if (!/^[0-9.]$/.test(inputChar)) {
             event.preventDefault();
+            return;
+        }
+
+        // Verificar si ya existe un punto decimal en el campo actual
+        const currentInputValue = (event.target as HTMLInputElement).value;
+        if (inputChar === '.' && currentInputValue.includes('.')) {
+            event.preventDefault(); // Bloquear la entrada de más de un punto decimal
+        }
+    }
+
+    // Permitir teclas de control en el teclado como borrar, flechas y enter
+    @HostListener('keydown', ['$event'])
+    onKeyDown(event: KeyboardEvent) {
+        if ([
+            'Backspace', 'ArrowLeft', 'ArrowRight', 'Tab', 'Delete', 'Enter'
+        ].indexOf(event.key) !== -1) {
+            return; // Permitir estas teclas
         }
     }
 }
